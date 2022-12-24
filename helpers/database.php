b434889539233c1370f35b717b8d6e2428cc8978
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-12-23 23:22:01 -0600

    Create(src/helpers/database.php):create conection to database with API  mysqli

--- helpers/database.php
+++ helpers/database.php
@@ -7,7 +7,7 @@ $dbname = 'glucosaday';
 
 // Crea la conexión
 //$conn = new mysqli($host, $user, $password, $dbname);
-$conn = mysqli_connect($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 // Verifica si la conexión fue exitosa
 // if ($conn->connect_error) {
@@ -22,27 +22,3 @@ echo $response;
 
 // Cierra la conexión
 mysqli_close($conn);
-?>
-
-
-
-<?php
-$db = new PDO("mysql:host=localhost;dbname=my_guitar_shop1", "root", "");
-// set the PDO error mode to exception
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	if (isset($_POST['submit'])) { //check if form was submitted
-
-		try{
-            $stmt = $db->prepare('INSERT INTO guitar (manufacturer, model, year) VALUES (:manufacturer, :model, :year)');
-            $stmt->execute(array(':manufacturer' => $_POST['manufacturer'], ':model' => $_POST['model'], ':year' => $_POST['year']));
-
-            echo '<script type="text/javascript">'; 
-            echo 'alert("Guitar Added");'; 
-            echo 'window.location.href = "index.php";';  
-        }
-        catch{
-            echo 'error';
-        }
-    }
-?> 
