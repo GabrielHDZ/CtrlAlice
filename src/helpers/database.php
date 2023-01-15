4f74cdcdabe9155f394df08c74a732637e0661c7
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-01-14 23:55:13 -0600

    Modify <./src/helpers/database.php>::transfor connection to class

--- src/helpers/database.php
+++ src/helpers/database.php
@@ -1,24 +1,35 @@
 <?php
-// Datos de conexión a la base de datos
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'glucosaday';
+namespace Gabriel\ServerTienda\helpers
 
-// Crea la conexión
-//$conn = new mysqli($host, $user, $password, $dbname);
-$conn = new mysqli($host, $user, $password, $dbname);
+class Database{
 
-// Verifica si la conexión fue exitosa
-// if ($conn->connect_error) {
-if (!$conn) {
-    die("Conexión fallida: " . mysqli_connect_error());
+    // Datos de conexión a la base de datos
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbname;
+    
+    public function __constructor(){
+        $this->host='localhost';
+        $this->user='root';
+        $this->password='';
+        $this->dbname='glucosaday';
+    }
+    // Crea la conexión
+    //$conn = new mysqli($host, $user, $password, $dbname);
+    $conn = new mysqli($host, $user, $password, $dbname);
+    
+    // Verifica si la conexión fue exitosa
+    // if ($conn->connect_error) {
+    if (!$conn) {
+        die("Conexión fallida: " . mysqli_connect_error());
+    }
+    echo "Conexión exitosa";
+    
+    //buscar base
+    $response = mysqli_select_db($conn, 'glucosaday');
+    echo $response;
+    
+    // Cierra la conexión
+    mysqli_close($conn);
 }
-echo "Conexión exitosa";
-
-//buscar base
-$response = mysqli_select_db($conn, 'glucosaday');
-echo $response;
-
-// Cierra la conexión
-mysqli_close($conn);
