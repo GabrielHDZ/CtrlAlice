65cd21f187d954d760b6bcda170129ef8d3829e8
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-01-17 00:37:36 -0600

    Modify <src/model/Oxigenacion.php>::Add methods in class oxigeno in Oxigenacion.php for the manipulation data CRUD

--- src/model/Oxigenacion.php
+++ src/model/Oxigenacion.php
@@ -3,17 +3,16 @@
 namespace Gabriel\ServerTienda\model;
 
 use Gabriel\ServerTienda\helpers\Database;
+use PDO;
 
-class Oxigeno extends Database
-{
+class Oxigeno extends Database{
     private string $uuid;
     private string $id_paciente;
     private int $spo;
     private int $pr;
 
     //constructor
-    public function __construct(string $id_paciente, int $spo,int $pr)
-    {
+    public function __construct(string $id_paciente, int $spo,int $pr){
         parent::__construct();
         $this->uuid = uniqid();
         $this->id_paciente = $id_paciente;
@@ -29,6 +28,31 @@ class Oxigeno extends Database
             'pr'=>$this->pr
         ]);
     }
+    public function update(){
+        $query=$this->connect()->prepare("UPDATE test_oxigeno SET id_paciente=:id_paciente,fecha=NOW(),spo=:spo,pr=:pr WHERE id=:uuid");
+        $query->execute([
+            'uuid'=>$this->uuid,
+            'id_paciente'=>$this->id_paciente,
+            'spo'=>$this->spo,
+            'pr'=>$this->pr
+        ]);
+    }
+
+    //get static
+    public static funcion get($uuid){
+        $db=new Database();
+        $query=$db->connect()->prepare("SELECT * FROM test_oxigeno WHERE id=:uuid");
+        $query->execute(['uuid'=>$uuid]);
+        $oxigeno=Oxigeno::createFromArray($query->fetch(PDO::FETCH_ASSOC));
+    }
+    public static createFromArray($arr):Oxigeno{
+        $oxigeno=new Oxigeno($arr['id_paciente'],$arr['spo'],$arr['pr']);
+        $oxigeno->setUUID($arr['id']);
+
+        return $oxigeno;
+    }
+
+
 
     public function getUUID(){
         return $this->uuid;
@@ -36,18 +60,21 @@ class Oxigeno extends Database
     public function setUUID($value){
         $this->uuid=$value;
     }
+
     public function getIDPaciente(){
         return $this->id_paciente;
     }
     public function setIDPaciente($value){
         $this->id_paciente=$value;
     }
+
     public function getSPO(){
         return $this->spo;
     }
     public function setSPO($value){
         $this->spo=$value;
     }
+
     public function getPR(){
         return $this->pr;
     }
