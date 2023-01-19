4bf41c08a5a008976b84b27470e1382bdd9aff0e
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-01-18 23:45:22 -0600

    Add <src/views/create_user.php>::Add the html tags for input data

--- src/views/create_user.php
+++ src/views/create_user.php
@@ -2,5 +2,46 @@
 
 use Gabriel\ServerTienda\models\User;
 
-$person = new User('Alicia', 'Hernandez', 67, 'she is a my mother and is the best mom for ever', 1);
-$person->save();
+if (count($_POST) > 0) {
+    $firstname = isset($_POST['txt_first']) ?? '';
+    $lastname = isset($_POST['txt_last']) ?? '';
+    $age = isset($_POST['txt_age']) ?? '';
+    $description = isset($_POST['txt_desc']) ?? '';
+    $status = isset($_POST['txt_state']) ?? '';
+
+    $person = new User($firstname, $lastname, $age, $description, $status);
+    $person->save();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-git scale=1.0">
+    <title>Users</title>
+</head>
+
+<body>
+    <form action="?view=create_user" method="POST">
+        <label for="txt_first">Nombre del paciente</label>
+        <input type="text" name="txt_first">
+
+        <label for="txt_last">Apellidos del paciente</label>
+        <input type="text" name="txt_last">
+
+        <label for="txt_age">edad del paciente</label>
+        <input type="number" name="txt_age">
+
+        <label for="txt_desc">Describe brevemente al paciente</label>
+        <input type="text" name="txt_desc">
+
+        <label for="txt_state">Persona Activa</label>
+        <input type="number" name="txt_state">
+
+        <input type="submit" value="guardar">
+    </form>
+</body>
+
+</html>
--- src/views/home.php
+++ src/views/home.php
@@ -5,15 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://unpkg.com/htmx.org@1.8.4" integrity="sha384-wg5Y/JwF7VxGk4zLsJEcAojRtlVp1FKKdGy1qN+OMtdq72WRvX/EdRdqg/LOhYeV" crossorigin="anonymous"></script>
-
     <title>Home</title>
 </head>
 
 <body>
-    <div hx-put="/messages">
-    Put To Messages
-</div>
 
 
 </body>
