ef80984b49df4f74d28ee7fff40860edbbe5a760
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-04-12 23:06:29 -0600

    Update<functions.js>::more css.

--- src/views/user/functions.js
+++ src/views/user/functions.js
@@ -1,6 +1,5 @@
 function open_modal() {
-    let div = document.getElementById('modales');
-    div.style.display = "block";
+    let div = document.getElementById('modales').style.display = "block";
 }
 function close_modal() {
     let div = document.getElementById('modales');
