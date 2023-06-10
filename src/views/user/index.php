1a36b3fa6d07c9dc0a28e3099872e4bfd6d432e4
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-06-09 22:47:21 -0600

    save

--- src/views/user/index.php
+++ src/views/user/index.php
@@ -1,10 +1,11 @@
+<!-- SCRIPT -->
 <?php
-
 use Gabriel\ServerTienda\models\User;
 
 $pacientes = User::get_data_all_pacientes();
 ?>
 
+<!-- TEMPLATE -->
 <?php include('src/views/components/nav.php') ?>
 <section id="pacientes">
     <aside>
@@ -22,35 +23,40 @@ $pacientes = User::get_data_all_pacientes();
                 </details><button class="button is-primary">Buscar</button>
             </li>
         </ul>
-
     </aside>
     <div id="modales">
         <div>
             <h1>Formulario para nuevos pacientes</h1>
             <button onclick="open_close_modal()">Close modal</button>
         </div>
-
     </div>
     <section>
         <?php foreach ($pacientes as $index => $paciente) { ?>
-            <article>
-                <header>
-                    <h1><span>Num.
-                            <?php echo $paciente->get_id() ?>
-                        </span>
-                        <?php echo $paciente->getFirst_name() ?>
-                    </h1>
-                </header>
-                <section>
-                    <p>aqui va una pequeña informacion respecto al paciente, mas informacion darle al boton de "mas
-                        informacion"
-                    </p>
-                </section>
-                <footer>
-                    <a href="?view=user&option=update&id=<?php echo $paciente->get_id(); ?>">
-                        <span>More info</span>
-                    </a>
-                </footer>
+            <article class="card">
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">
+                                <?php echo $paciente->getFirst_name() ?>
+                            </p>
+                            <p class="subtitle is-6">
+                                <?php echo $paciente->getLast_name() ?>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                        <a href="#">#css</a> <a href="#">#responsive</a>
+                        <br>
+                        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    </div>
+                </div>
             </article>
         <?php } ?>
     </section>
